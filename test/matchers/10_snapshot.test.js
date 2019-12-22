f220cc51e186f24453f8f930acc02352eb32ae98
LiYanLance <leeyanly@163.com>
2019-12-22 22:55:26 +0800

    custom snapshot

--- test/matchers/10_snapshot.test.js
+++ test/matchers/10_snapshot.test.js
@@ -45,4 +45,29 @@ describe("SnapShot Matchers", () => {
       id: expect.any(Number),
     });
   })
+  
+
+  describe("customize snapshot", () => {
+    
+    const { toMatchSnapshot } = require("jest-snapshot")
+    
+    expect.extend({
+      toMatchTrimmedSnapshot(received) {
+        delete received.props.className
+        return toMatchSnapshot.call(this, received, 'toMatchTrimmedSnapshot')
+      }
+    })
+    
+    it("snapshot without classname and spaces", () => {
+      const SomeComponent = ({ content }) => (
+        <div className="some class">{content}</div>
+      );
+  
+      const tree = renderer
+      .create(<SomeComponent content="dummy content" />)
+      .toJSON();
+
+      expect(tree).toMatchTrimmedSnapshot()
+    })
+  })
 });
